1b2b007c4bf840231153f21cd495d2f81d34ad47
wondokim <wondokim@wondokimui-MacBookAir.local>
2023-11-23 19:25:23 +0900

    dev-order

---
+++ src/main/java/com/example/demo10/Member/Entity/Member.java
@@ -0,0 +1,43 @@
+package com.example.demo10.Member.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column
+    @Setter
+    private String name;
+
+    @Column
+    @Setter
+    private String address;
+
+//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+//    private List<Order> orders = new ArrayList<>();
+//
+//
+//    public void addMember(Order order){
+//        orders.add(order);
+//        order.setMember(this);
+//    }
+
+
+
+}
